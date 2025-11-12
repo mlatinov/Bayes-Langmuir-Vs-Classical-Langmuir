f6033b5ccf0017264c30c17192c86c034a10c4ba
mlatinov <latinovmetodi@gmail.com>
2025-11-12 21:28:09 +0300

    add more data and extend the pipeline to include multilevel models and experiment with the priors

--- _targets.R
+++ _targets.R
@@ -9,33 +9,70 @@ tar_source("scripts/functions/")
 #### Targets Pipeline ####
 list(
 
-  #### Load the data ####
+  #### Load the datasets ####
+
+  # First Replication Used also for the Bayesian Model
   tar_target(
     name = data_iso,
     command = read_excel("data/iso 1.xlsx"),
     packages = "readxl"
   ),
 
+  # Second Replication
+  tar_target(
+    name = data_iso_2,
+    command = read_excel("data/iso_2.xlsx"),
+    packages = "readxl"
+  ),
+
+  # Third Replication
+  tar_target(
+    name = data_iso_3,
+    command = read_excel("data/iso_3.xlsx"),
+    packages = "readxl"
+  ),
+
+  # Forth Replication
+  tar_target(
+    name = data_iso_4,
+    command = read_excel("data/iso_4.xlsx"),
+    packages = "readxl"
+  ),
+
   ##### Clean the data ####
   tar_target(
     name = clean_iso,
-    command = clean_iso_data(data_iso)
+    command = clean_iso_data(
+      datasets = list(
+        data_iso,
+        data_iso_2,
+        data_iso_3,
+        data_iso_4
+      )),
+    packages = "janitor"
     ),
 
-  #### Classical approach ####--------------------------------
-
-  ##### Plot the isotherm ####
+  ##### Combine the Clean Datasets into one with experiment id column ####
   tar_target(
-    name = iso_plot,
-    command = plot_iso(clean_iso),
-    packages = "ggpmisc"
+    name = combined_data,
+    command = combine_datasets(clean_iso)
   ),
+
+  #### Classical approach ####--------------------------------
 
   ##### Calculate molecular surface ####
   tar_target(
     name = molecular_surface,
-    command = calc_molecular_surface(clean_iso)
+    command = calc_molecular_surface(as.data.frame(clean_iso[1]))
   ),
+
+  ##### Plot the isotherm ####
+  tar_target(
+    name = iso_plot,
+    command = plot_iso(molecular_surface$full_curve),
+    packages = "ggpmisc"
+  ),
+
 
   #### Bayesian approach ####--------------------------------
 
@@ -43,12 +80,12 @@ list(
   tar_target(
     name = pp_model_simulation,
     command = prior_simulation(
-      data = clean_iso,
+      data = as.data.frame(clean_iso[1]),
       #### Priors ####
       priors = c(
-        prior(normal(50, 10), nlpar = "pimax", lb = 40), # Prior for max pressure
-        prior(normal(45, 10), nlpar = "Amol", lb = 20),  # Prior for molecular surface area
-        prior(exponential(1), class = "sigma")           # Prior for the error
+        prior(normal(50, 10), nlpar = "pimax", lb = 10,ub = 100),  # Prior for max pressure
+        prior(normal(45, 10), nlpar = "Amol", lb = 10, ub = 100),  # Prior for molecular surface area
+        prior(exponential(1), class = "sigma")                     # Prior for the error
       )
     )
   ),
@@ -63,12 +100,12 @@ list(
   tar_target(
     name = bayesian_model,
     command = bayesian_modeling(
-      data = clean_iso,
+      data = as.data.frame(clean_iso[1]),
       #### Priors ####
       priors = c(
-        prior(normal(50, 10), nlpar = "pimax", lb = 40), # Prior for max pressure
-        prior(normal(45, 10), nlpar = "Amol", lb = 20),  # Prior for molecular surface area
-        prior(exponential(1), class = "sigma")           # Prior for the error
+        prior(normal(50, 10), nlpar = "pimax", lb = 10, ub = 100), # Prior for max pressure
+        prior(normal(45, 10), nlpar = "Amol", lb = 10, ub = 100),  # Prior for molecular surface area
+        prior(exponential(1), class = "sigma")                     # Prior for the error
         )
       )
     ),
@@ -83,5 +120,75 @@ list(
   tar_target(
     name = bayesian_results,
     command = bayes_insights(bayesian_model)
+  ),
+
+  ##### Prior Predictive simulation for hierarchical model ####
+  tar_target(
+    name = pp_model_simulation_h,
+    command = prior_simulation_h(
+      data = combined_data,
+      #### Priors ####
+      priors = c(
+
+        ## Residual error: typical measurement noise around 1 mN/m
+        prior(exponential(1), class = "sigma"),
+
+        ## Amol: typical molecular area ~45 mm² (range ~25-65 mm²)
+        prior(normal(45,10), class = "b",nlpar = "Amol",lb = 10, ub = 100),
+
+        ## Between-experiment variation in molecular area: ~1 mm²
+        prior(exponential(1),class = "sd",nlpar = "Amol",group = "experiment_id"),
+
+        ## pimax: typical maximum pressure ~50 mN/m (range ~30-70 mN/m)
+        prior(normal(50, 10), nlpar = "pimax", lb = 10, ub = 100),
+
+        ## Between-experiment variation in max pressure: ~1 mN/m
+        prior(exponential(1),class = "sd",nlpar = "pimax",group = "experiment_id")
+      )
+    )
+  ),
+
+  ##### Diagnostics of Prior Predictive simulation for hierarchical model ####
+  tar_target(
+    name = prior_simulation_diagnostics_h,
+    command = prior_sim_diagnose(pp_model_simulation_h)
+  ),
+
+  #### Bayesian hierarchical model ####
+  tar_target(
+    name = hierarchical_model,
+    command = hierarchical_modeling(
+      data = combined_data,
+      #### Priors ####
+      priors = c(
+
+        ## Residual error: typical measurement noise around 1 mN/m
+        prior(exponential(1), class = "sigma"),
+
+        ## Amol: typical molecular area ~45 mm² (range ~25-65 mm²)
+        prior(normal(45,10), class = "b",nlpar = "Amol",lb = 10, ub = 100),
+
+        ## Between-experiment variation in molecular area: ~2.5 mm²
+        prior(exponential(1),class = "sd",nlpar = "Amol",group = "experiment_id"),
+
+        ## Pimax: typical maximum pressure ~50 mN/m (range ~30-70 mN/m)
+        prior(normal(50, 10), nlpar = "pimax", lb = 10, ub = 100),
+
+        ## Between-experiment variation in max pressure: ~1 mN/m
+        prior(exponential(1),class = "sd",nlpar = "pimax",group = "experiment_id")
+      )
+    )
+  ),
+
+  ##### Bayesian model diagnostics for hierarchical model  ####
+  tar_target(
+    name = bayesian_model_diagnostics_h,
+    command = bayes_diagnose(hierarchical_model)
+  ),
+
+  ##### Bayesian Model Insights ####
+  tar_target(
+    name = bayesian_results_h,
+    command = bayes_insights(hierarchical_model)
     )
 )
